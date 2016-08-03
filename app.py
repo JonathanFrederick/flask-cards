14d607d7e5d22de2b3d738051e1e2ed8e91530bc
Jonathan Frederick <jrfreder@gmail.com>
2016-08-02 20:01:09 -0400

    registering users

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import os
-from flask import Flask
+from flask import Flask, request, abort, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-import models
-
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+import models
+
 
 @app.route('/')
 def home():
     return 'Hello World'
+
+
+@app.route('/api/user/', methods=['POST'])
+def register():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(make_response("Missing parameters", 400))
+    if models.User.query.filter_by(username=username).first():
+        abort(make_response("Username already exists", 400))
+    user = models.User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'username': user.username}), 201
 
 if __name__ == '__main__':
     app.run()
